3e581240ad3a47c882050011fe6ef2a93076c18d
Angelo Lima <angelolimadias@gmail.com>
2018-09-25 17:37:14 -0300

    Moda

---
+++ Modal/js/script.js
@@ -0,0 +1,20 @@
+var modal = document.querySelector('.container-modal');
+
+var openmodal = document.querySelector('.open-modal');
+
+var fechamodal = document.querySelector('.back-modal');
+
+openmodal.addEventListener('click', function () {
+    modal.classList.toggle('open');
+    // conmodal.classList.toggle('open');
+})
+
+openmodal.addEventListener('click', function () {
+    fechamodal.classList.add('open');
+
+});
+
+fechamodal.addEventListener('click', function(){
+    modal.classList.remove('open');
+    fechamodal.classList.remove('open');
+})
