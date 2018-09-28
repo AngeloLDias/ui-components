79439470ce472414bd99c2fdff68d44a69f183aa
Angelo Lima <angelolimadias@gmail.com>
2018-09-28 07:39:56 -0300

    site de roupa

--- Dropdown/exe1/js/script.js
+++ Dropdown/exe1/js/script.js
@@ -1,13 +1,34 @@
-
 // EXEMPLO DE DROPDOWN 1 
 
-var dropdown = document.querySelector('#dropdown1')
-var itens1 = document.querySelector('.itens1')
+var camisetasdrop = document.querySelector('.camisetasDropdown');
+var moletonsdrop = document.querySelector('.moletonsDropdown');
+var mangalongadrop = document.querySelector('.mangalongaDropdown');
+var batasdrop = document.querySelector('.batasDropdown');
+var almofadasdrop = document.querySelector('.almofadasDropdown');
+var posterdrop = document.querySelector('.posterDropdown');
 
-dropdown.addEventListener('click', function () {
-    itens1.classList.toggle('open')
+var catcamisetas = document.querySelector('.cat-camisetas');
+var catmoletons = document.querySelector('.cat-moletons');
+var catmangalongas = document.querySelector('.cat-mangalonga');
+var catbatas = document.querySelector('.cat-batas');
+var catalmofadas = document.querySelector('.cat-almofadas');
+var catposter = document.querySelector('.cat-poster');
 
+camisetasdrop.addEventListener('click', function () {
+    catcamisetas.classList.toggle('open');
 });
-
-
-
+moletonsdrop.addEventListener('click', function () {
+    catmoletons.classList.toggle('open');
+});
+mangalongadrop.addEventListener('click', function () {
+    catmangalongas.classList.toggle('open');
+});
+batasdrop.addEventListener('click', function () {
+    catbatas.classList.toggle('open');
+});
+almofadasdrop.addEventListener('click', function () {
+    catalmofadas.classList.toggle('open');
+});
+posterdrop.addEventListener('click', function () {
+    catposter.classList.toggle('open');
+});
